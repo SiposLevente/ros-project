ed6bb0949184d2324f88dd2601d8c1666776e2fd
SiposLevente <sipi.levente@gmail.com>
2022-03-11 20:06:17 +0100

    removed test method, added straight movement funcion

--- src/platypous_controller.py
+++ src/platypous_controller.py
@@ -1,7 +1,6 @@
 import rospy
 import math
 from geometry_msgs.msg import Twist
-from nav_msgs.msg import OccupancyGrid
 
 
 class platypous_controller:
@@ -9,27 +8,20 @@ class platypous_controller:
         rospy.init_node('platypous_controller', anonymous=True)
         self.twist_pub = rospy.Publisher(
             '/cmd_vel/nav', Twist, queue_size=10)
-        self.slam_listener = rospy.Subscriber(name, OccupancyGrid)
 
-    def test(self, forward):
+    def move_straight(self, speed_m_per_s, time_sec, forward=True):
         vel_msg = Twist()
         if forward:
-            vel_msg.linear.x = 2
-            vel_msg.angular.z = 2
+            vel_msg.linear.x = speed_m_per_s
         else:
-            vel_msg.linear.x = -2
-            vel_msg.angular.z = -2
-
-        rate = rospy.Rate(100)
+            vel_msg.linear.x = -speed_m_per_s
         self.twist_pub.publish(vel_msg)
 
+        rate = rospy.Rate(100)
         t0 = rospy.Time().now().to_sec()
-
-        while(rospy.Time().now().to_sec() - t0 <= 2):
-            print(forward)
+        while (rospy.Time().now().to_sec() - t0 <= time_sec) and not (rospy.is_shutdown()):
             self.twist_pub.publish(vel_msg)
             rate.sleep()
-
         vel_msg.linear.x = 0
         self.twist_pub.publish(vel_msg)
 
@@ -37,9 +29,4 @@ class platypous_controller:
 if __name__ == '__main__':
     # Init
     pc = platypous_controller()
-    toggler = False
-    while(True):
-        pc.test(toggler)
-        toggler = not toggler
-
-    # Send turtle on a straight line
+    pc.move_straight(speed_m_per_s=1, time_sec=1, forward=True)
